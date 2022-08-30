b34a3df6c91c2298081c9dd783463d5579644e71
Edivansanto5 <edivansantos1478@gmail.com>
2022-08-30 09:47:25 -0300

    fazendo uma fverificação onde vai procurar se houve algum erro ou não, caso dê erro vai cair no cat automaticamente

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ let fileContent;
 const someMath = 2 + 3;
 
 try{
-
-}catch{
-    
+    fileContent = fs.readFileSync('big-filetext','utf-8');
+    console.log('file has been red');
+}catch(err){
+    console.log('!ERRO');
 }
+
+const text = ` the num is  ${someMath}`
