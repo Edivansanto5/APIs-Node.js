a13f039f1435af215ac1837f17e8581f22d344f0
Edivansanto5 <edivansantos1478@gmail.com>
2022-08-31 15:57:52 -0300

    finalizando a aula de call-stack

--- Call-Stack/app.js
+++ Call-Stack/app.js
@@ -1,1 +1,10 @@
-console.log()
+function generacao(age){
+    return 2016 - age
+}
+
+function generacaoDescricao(name,surName,age){
+    const fullName = `${name},${surName}`
+    const bornDate = generacao(age)
+
+    return `${fullName} is ${age} old and born in ${bornDate}`
+}
